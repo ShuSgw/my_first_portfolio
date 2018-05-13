8f5133deb9036028d9f36a189f818253a7cb66ea
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2018-05-13 01:14:16 -0700

    design chnaged with css comp

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+  $("html").animsition({
+    inClass: 'flip-in-y',
+    outClass: 'flip-out-y',
+    inDuration: 1000,
+    outDuration: 1000,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'black', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: true,
+    touchSupport:true,
+    timeoutCountdown: 500,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    overlay : false,
+    overlayParentElement : 'body',
+  });
+});
