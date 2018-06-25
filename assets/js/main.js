02b60f84038486ca6a309f0bf3c19fe7a4adcf9d
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2018-06-25 11:41:32 -0700

    comp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,18 @@
-$(function(){
-  $("#transition").animsition({
-    inClass: 'flip-in-y',
-    outClass: 'flip-out-y',
-    inDuration: 1000,
-    outDuration: 1000,
+$(document).ready(function() {
+  $(".animation").animsition({
+    inClass: 'flip-in-y-fr',
+    outClass: 'flip-out-y-fr',
+    inDuration: 800,
+    outDuration: 800,
     linkElement: '.animsition-link',
     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
     loading: true,
-    loadingParentElement: 'black', //animsition wrapper element
+    loadingParentElement: 'body', //animsition wrapper element
     loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
+    loadingInner: '<img id="loadingGif" src="assets/img/loading.gif"/>', // e.g '<img src="loading.svg" />'
     timeout: true,
-    touchSupport:true,
     timeoutCountdown: 500,
-    onLoadEvent: true,
+    onLoadEvent: false,
     browser: [ 'animation-duration', '-webkit-animation-duration'],
-    overlay : false,
-    overlayParentElement : 'body',
   });
 });
